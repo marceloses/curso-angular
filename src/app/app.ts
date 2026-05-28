f6eddaa851602ffecd93f4870b93e639aad96ec7
marceloses <marceloses@gmail.com>
2026-05-27 23:14:09 -0300

    Inicio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Calculadora } from "./calculadora/calculadora";
 
-@Component({
+@Component({    
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Calculadora],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('curso-angular');
+  title = 'curso-angular';
 }
---
+++ src/app/olaMundo/OlaMundo.Component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-ola-mundo',
+  template: '<h1>Ola Mundo tudo bem?</h1>'
+  
+})
+export class OlaMundoComponent {
+
+}
